1206b56c96d1df58725ea8d0159e7449a3c6f899
vvitoriaddanielads13sp <vitoriadaniela97@gmail.com>
2019-11-05 22:28:15 -0200

    topy

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace show
+{
+    class Program
+    {
+     public static int Soma (int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
