c5ee9db1365bb16928b3d36a7b6d6fd164efb122
mathsluz97 <maths.luzsantana97@gmail.com>
2019-11-05 23:18:32 -0200

    boa

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace show
         {
             Console.WriteLine("Resultado da soma: {0}+{1}",3,5,Somar(3,5));
             Console.WriteLine("Resultado da subtração: {0} + {1}", 3, 5, Subtrair(3, 5));
+            Console.WriteLine("Resultado da Multiplicação: {0} + {1} = {2}", 3, 5, multiplicar(3, 5));
         }
 
     }
